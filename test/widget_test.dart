82de23942f0e312371d1de745a5945e2a35dd5fa
ryantowns-blip <ryan.towns@gmail.com>
2026-09-09 17:46:35 -0500

    Test WMT login UI flow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,25 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text('ATC Schedule Manager'), findsOneWidget);
+    expect(find.text('WMT Scheduler'), findsOneWidget);
+    expect(find.text('Connect to WMT'), findsOneWidget);
     expect(find.text('Parse schedule'), findsOneWidget);
+  });
+
+  testWidgets('WMT login advances from email to MyAccess password step',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const AtcScheduleManagerApp());
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Connect to WMT'));
+    await tester.pumpAndSettle();
+    expect(find.textContaining('Step 1 of 2'), findsOneWidget);
+
+    await tester.enterText(find.byType(EditableText).first, 'controller@faa.gov');
+    await tester.tap(find.text('Continue'));
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('Step 2 of 2'), findsOneWidget);
+    expect(find.text('Continue in MyAccess'), findsOneWidget);
   });
 }
