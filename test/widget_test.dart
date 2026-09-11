14774f2335e5b7fbca10900516d21fb6b2c8645d
ryantowns-blip <ryan.towns@gmail.com>
2026-09-11 13:03:32 -0500

    Update widget test for Lite app identity

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@ void main() {
     SharedPreferences.setMockInitialValues({});
   });
 
-  testWidgets('ATC Schedule Manager opens to screenshot import home',
+  testWidgets('ATC Schedule Manager Lite opens to screenshot import home',
       (WidgetTester tester) async {
     await tester.pumpWidget(const AtcScheduleManagerApp());
     await tester.pumpAndSettle();
 
-    expect(find.text('ATC Schedule Manager'), findsOneWidget);
+    expect(find.text('ATC Schedule Manager Lite'), findsOneWidget);
     expect(find.text('No schedule imported yet'), findsOneWidget);
     expect(find.text('Import Schedule Screenshots'), findsOneWidget);
     expect(find.text('Upcoming Leave'), findsOneWidget);
